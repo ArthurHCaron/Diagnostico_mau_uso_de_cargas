8c0399df2414ef186b3c0c96f9fdc1ceeac1e92b
ArthurHCaron <arthurhcaron@gmail.com>
2026-03-09 11:49:22 -0300

    corrigido

--- src/main.cpp
+++ src/main.cpp
@@ -26,16 +26,13 @@ BaseType_t taskCaptacaoDados = pdFALSE,
 SemaphoreHandle_t dadosRelevantes;
 
 volatile struct{
-  float tensao,
-        tensao2,
-        pico,
-        media,
-        eficaz;
+  float tensao2,
+        corrente2,
+        prodVI;
   int contador;
 } dados;
 
 void initDados(void);
-void zerar(void);
 static void IRAM_ATTR Timer0_ISR();
 static void IRAM_ATTR Timer1_ISR();
 static void taskCore0(void* pvParameters);
@@ -47,8 +44,9 @@ void setup(){
   initDados();
 
   adc1_config_width(ADC_WIDTH_BIT_12);
-  adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11);
-  tipo = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, ADC_VREF, &adc);
+  adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_11db);
+  adc1_config_channel_atten(ADC1_CHANNEL_9, ADC_ATTEN_11db);
+  tipo = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_11db, ADC_WIDTH_BIT_12, ADC_VREF, &adc);
 
   dadosRelevantes = xSemaphoreCreateBinary();
   xSemaphoreGive(dadosRelevantes);
@@ -65,7 +63,7 @@ void setup(){
   xTaskCreatePinnedToCore(
     taskCore0,
     "Leitura de dados",
-    1000,
+    5000,
     NULL,
     1,
     &captacaoDados,
@@ -112,29 +110,37 @@ static void IRAM_ATTR Timer1_ISR(){
 static void taskCore0(void* pvParameters){
   const float offset = 1.64,
               kPropor = 1001.922667;
-  int leituraRaw = 0;
-  static float sumV = 0,
-               sumV2 = 0,
+  int leituraRaw = 0,
+      correnteRaw = 0;
+  static float sumV2 = 0,
+               sumI2 = 0,
                cont = 0,
-               leituraVolt = 0;
+               leituraVolt = 0,
+               leituraAmp = 0,
+               prod = 0;
 
   while(1){
     ulTaskNotifyTake(taskCaptacaoDados, portMAX_DELAY);
 
     leituraRaw = adc1_get_raw(ADC1_CHANNEL_3);
+    correnteRaw = adc1_get_raw(ADC1_CHANNEL_9);
     leituraVolt = esp_adc_cal_raw_to_voltage(leituraRaw, &adc) / 1000.0;
     leituraVolt = kPropor * (leituraVolt - offset);
-    sumV += leituraVolt;
+    leituraAmp = 20.0 * (esp_adc_cal_raw_to_voltage(correnteRaw, &adc) / 1000.0 - 1.65);
     sumV2 += leituraVolt * leituraVolt;
+    sumI2 += leituraAmp * leituraAmp;
+    prod += leituraVolt * leituraAmp;
     cont++;
 
     if(xSemaphoreTake(dadosRelevantes, 0) == pdPASS){
       dados.tensao2 += sumV2;
-      dados.tensao += sumV;
+      dados.corrente2 += sumI2;
       dados.contador += cont;
-      sumV = 0;
+      dados.prodVI += prod;
       sumV2 = 0;
+      sumI2 = 0;
       cont = 0;
+      prod = 0;
       xSemaphoreGive(dadosRelevantes);
     }
   }
@@ -142,7 +148,12 @@ static void taskCore0(void* pvParameters){
 
 static void taskCore1(void* pvParameters){
   int codHttp = 0;
-  float eficazEnvio = 0;
+  float envio[5],
+        vEficaz = 0,
+        iEficaz = 0,
+        potAp = 0,
+        potAt = 0,
+        potR = 0;
   String url = "";
   HTTPClient http;
 
@@ -150,44 +161,42 @@ static void taskCore1(void* pvParameters){
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
     xSemaphoreTake(dadosRelevantes, portMAX_DELAY);
 
-    dados.eficaz = sqrtf(dados.tensao2 / dados.contador);
-    dados.media = dados.tensao / dados.contador;
-    dados.pico = dados.eficaz * M_SQRT2 / 2;
-    eficazEnvio = dados.eficaz;
-    zerar();
-
-    Serial.printf("E = %.2f\n", dados.eficaz);
-    Serial.printf("M = %.2f\n", dados.media);
-    Serial.printf("P = %.2f\n", dados.pico);
+    vEficaz = sqrtf(dados.tensao2 / dados.contador);
+    iEficaz = sqrtf(dados.corrente2 / dados.contador);
+    potAp = iEficaz * vEficaz;
+    potR = dados.prodVI / dados.contador;
+    potAt = sqrtf(potAp * potAp - potR * potR);
+    envio[0] = vEficaz;
+    envio[1] = iEficaz;
+    envio[2] = potAt;
+    envio[3] = potR;
+    envio[4] = potAp;
+    initDados();
     
     xSemaphoreGive(dadosRelevantes);
 
-    if(!isnan(dados.eficaz)){
+    if(!isnan(vEficaz)){
       if(WiFi.status() == WL_CONNECTED){
         url = server + "?api_key=" + chave_api + 
-              "&field1=" + String(eficazEnvio, 2);
+              "&field1=" + String(envio[0], 2) + 
+              "&field2=" + String(envio[1], 2) +
+              "&field3=" + String(envio[2], 2) + 
+              "&field4=" + String(envio[3], 2) +
+              "&field5=" + String(envio[4], 2);
         http.begin(url);
 
         codHttp = http.GET();
-        Serial.println(codHttp > 0 ? "Dados enviados" : "Erro de envio WiFi");
-
         http.end();
       }
+      //V_rms;I_rms;Potencia_ativa;Potencia_reativa;Potencia_aparente;Envio
+      Serial.printf("%.2f;%.2f;%.2f;%.2f;%.2f;%d\n", envio[0], envio[1], envio[2], envio[3], envio[4], codHttp);
     }
   }
 }
 
 void initDados(void){
   dados.tensao2 = 0;
-  dados.tensao = 0;
-  dados.contador = 0;
-  dados.media = 0;
-  dados.eficaz = 0;
-  dados.pico = 0;
-}
-
-void zerar(void){
-  dados.tensao2 = 0;
-  dados.tensao = 0;
+  dados.corrente2 = 0;
+  dados.prodVI = 0;
   dados.contador = 0;
 }
